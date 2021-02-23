bb3944b0efd500b4655a79d4a01197422cb89b54
Gene7oh <gene7oh@hotmail.com>
2021-02-23 12:07:02 -0600

    Create index.php check for functionality of the editor

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    $message = "Hello World is this thing working?" . "<br>" . "It would be nice if it were!" . "<br>" . "Onna side note we need to see if the auto view works in the browser." . "<br>" . "Yea! It does!";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>rets home</title>
+</head>
+<body>
+<?php echo $message; ?>
+</body>
+</html>
